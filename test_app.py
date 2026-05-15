f900190ec5750a53035b5a940862b6df128d5493
Janaki Nath Verma <janakinathv@gmail.com>
2026-05-15 21:36:04 +0530

    Task 5

--- test_app.py
+++ test_app.py
@@ -1,16 +1,18 @@
+import pytest
 from app import app
 
+# Yahan koi fixture nahi hoga, sirf tests honge
 def test_header_exists(dash_duo):
-    # Start the app
     dash_duo.start_server(app)
-    
-    # Wait for the header element to appear
-    dash_duo.wait_for_element("h1", timeout=10)
-    
-    # Check if the text matches
-    assert dash_duo.find_element("h1").text == "Pink Morsel Sales Visualiser"
+    dash_duo.wait_for_element("h1", timeout=15)
+    assert dash_duo.find_element("h1").text.strip().upper() == "PINK MORSEL SALES ANALYSIS DASHBOARD"
 
 def test_visualization_exists(dash_duo):
     dash_duo.start_server(app)
-    dash_duo.wait_for_element("#sales-graph", timeout=10)
+    dash_duo.wait_for_element("#sales-graph", timeout=15)
     assert dash_duo.find_element("#sales-graph") is not None
+
+def test_region_picker_exists(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#region-picker", timeout=15)
+    assert dash_duo.find_element("#region-picker") is not None
